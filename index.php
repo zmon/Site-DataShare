b3d2f412e25ff54d0d46c3954d7faef8cfbcb6a4
Paul D Barham <paulb@savagesoft.com>
2015-10-24 20:41:34 -0500

    Added support for bootstrap and jquery

--- index.php
+++ index.php
@@ -15,8 +15,23 @@ DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Site-DataShare/');	// The 
 DEFINE('URLFORDOCSINREPO', URLFORREPO . "/blob/master/Doc/");	// Docs in the repo
 
 DEFINE('TEXTEDITWINDOWSIZE', 'cols=120 rows=30'); //Sets the size of the text edit window in both edit.php and openedit.php
+DEFINE('TEXT_EDIT_WINDOW_SIZE', 'cols=120 rows=30'); //Sets the size of the text edit window in both edit.php and openedit.php
 
 DEFINE('TEXTEDITAREASTYLE', 'font-size: 16px; padding:10px;'); //Sets the size of the text edit window in both edit.php and openedit.php
+DEFINE('TEXT_EDIT_AREA_STYLE', 'font-size: 16px; padding:10px;'); //Sets the size of the text edit window in both edit.php and openedit.php
+
+DEFINE('LANDING_MD', 'ZZZ/landing.md'); // The repo home
+
+DEFINE('SOURCE_TAB_MESSAGE', 'Source'); // Tab
+
+DEFINE('EDIT_TAB_MESSAGE', 'Edit'); // Tab
+
+DEFINE('COMPLETE_TAB_MESSAGE', 'Edit and Complete');    // Tab
+
+DEFINE('DOC_TAB_MESSAGE', 'Document');  // Tab
+
+DEFINE('PRINT_TAB_MESSAGE', 'Print');   // Tab
+
 
 include("cmacc_helpers.php");
 

0c3eb63ce8fc52fa9def536f02e7a4e530451f5c
Paul D Barham <paulb@savagesoft.com>
2015-11-11 22:51:32 -0600

    Added landing page

---
+++ Doc/ZZZ/landing.md
@@ -0,0 +1,3 @@
+
+
+Model.Root=<p align="center">Need to add content to ZZZ/landing.md for this page. <br><br><a href="/index.php?action=list&file=">documents</a>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 
 DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
 
-DEFINE('LIB_PATH', ROOT . '/vendor/CommonAccord/cmacc-ui/library'); // Create global variable to our library	
+DEFINE('LIB_PATH', ROOT . '/vendor/CommonAccord/cmacc-ui/library'); // Create global variable to our library
+
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_PATH . DIRECTORY_SEPARATOR);	// Include it in PHP include/require search
 
 DEFINE('ASSETS_PATH', 'visual');  // Create global variable to our css and js assets
